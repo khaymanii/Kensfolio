3cd56614920bbfa0036aedadec2a0441e7e6dbb2
unknown <khaymanii>
2026-01-03 12:07:02 +0100

    edit about section

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -1,22 +1,26 @@
 import "./about.css";
 import SocialContact from "../../common/social-contact/index";
+
 function About() {
   return (
     <div className="about">
       <div className="about-top">
         <div className="about-info">
-          Hello There ✋, <br />I am <span className="info-name">Ken</span>,
-          <br />A Frontend & DevOps Engineer with experience building
-          production-ready applications and automating deployment workflows.
+          Hello There 👋, <br />I am <span className="info-name">Ken</span>,
+          <br />a Frontend Engineer, DevOps Engineer, and Innovation Officer
+          focused on building production-ready applications, automating
+          deployment workflows, and driving impactful technology solutions.
         </div>
+
         <div className="about-photo">
           <img
             src={require("../../../assets/coding.png")}
             className="picture"
-            alt=""
+            alt="Coding illustration"
           />
         </div>
       </div>
+
       <SocialContact />
     </div>
   );
